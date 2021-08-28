433c8a0d6022f4c0b94a012154ccf1c3b5204dab
Loyisa <894857715@qq.com>
2021-08-28 10:08:54 +0800

    还是用recaptcha v2好了

--- loyisa_recaptcha_callback.php
+++ loyisa_recaptcha_callback.php
@@ -8,7 +8,6 @@ function callback_init()
 	// 插件配置
 	option::add('loyisa_recaptcha_register', 1);
 	option::add('loyisa_recaptcha_login', 1);
-	option::add('loyisa_recaptcha_score', 0.5);
 	option::add('loyisa_recaptcha_sitekey', '');
 	option::add('loyisa_recaptcha_secretkey', '');
 }
@@ -18,7 +17,6 @@ function callback_remove()
 	// 禁用插件时移除配置文件
 	option::del('loyisa_recaptcha_register');
 	option::del('loyisa_recaptcha_login');
-	option::del('loyisa_recaptcha_score');
 	option::del('loyisa_recaptcha_site_key');
 	option::del('loyisa_recaptcha_secret_key');
 }
